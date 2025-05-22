e93efa42c5636813ceae00924f4267ad7fa42290
Adrian-ortiz0 <dxniel7328@gmail.com>
2025-05-22 09:59:30 -0500

    feat: :sparkles: Custom visualization functionality

--- website/models.py
+++ website/models.py
@@ -11,8 +11,8 @@ class Record(models.Model):
     state = models.CharField(max_length=100)
     zipcode = models.CharField(max_length=20)
 
-def __str__(self):
-    return f"{self.first_name} {self.last_name}"
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
     
 
 # Create your models here.
--- website/views.py
+++ website/views.py
@@ -3,9 +3,11 @@ from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 
 def home(request):
+    records = Record.objects.all()
     # Check to see if logging in
     if request.method == 'POST':
         username = request.POST['username']
@@ -20,7 +22,7 @@ def home(request):
             messages.success(request, "There was an error logging in, please try again")
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records': records})
 
 def logout_user(request):
     logout(request)
@@ -42,4 +44,12 @@ def register_user(request):
         form = SignUpForm()
 
     return render(request, 'register.html', {'form': form})
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        customer_record =Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record': customer_record})
+    else:
+        messages.success(request, "You must be logged in to view this page")
+        return redirect('home')
 # Create your views here.
